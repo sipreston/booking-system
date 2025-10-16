790c11575d8b95992e75c06c1bec8a5a2335e033
Simon Preston <simon@simonpreston.net>
2025-10-16 14:33:00 +0100

    Update main seeder

--- application/database/seeders/DatabaseSeeder.php
+++ application/database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
             CountrySeeder::class,
             ExtrasSeeder::class,
             AmenitiesSeeder::class,
+            RoomTypeSeeder::class,
+            UnitTypeSeeder::class,
         ]);
     }
 }
